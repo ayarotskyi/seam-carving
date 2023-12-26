f388d2cd076c93feb3fb9383ff13e1cd684f45f3
Andrian Yarotskyi <9094533-kokot4n@users.noreply.gitlab.com>
2023-12-26 21:41:49 +0000

    feat: implemented vertical resizing

--- handlers/image.go
+++ handlers/image.go
@@ -2,11 +2,11 @@ package handlers
 
 import (
 	"image"
-	"image/color"
 	"image/png"
 	"math"
 	"net/http"
 	L "seam-carving/utils"
+	"strconv"
 )
 
 func Image_handler(w http.ResponseWriter, r *http.Request) {
@@ -15,105 +15,114 @@ func Image_handler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error decoding image: "+err.Error(), http.StatusBadRequest)
 		return
 	}
+	width, err := strconv.Atoi(r.FormValue("width"))
+	if err != nil {
+		http.Error(w, "Invalid width value", http.StatusBadRequest)
+	}
+	height, err := strconv.Atoi(r.FormValue("height"))
+	if err != nil {
+		http.Error(w, "Invalid height value", http.StatusBadRequest)
+	}
 
 	energies := make([][]float64, img.Bounds().Dx())
 	for i := range energies {
 		energies[i] = make([]float64, img.Bounds().Dy())
 	}
 
-	max := 0.0
-	for i := 0; i < len(energies); i++ {
-		for j := 0; j < len(energies[0]); j++ {
-			energy := L.ComputeEnergy(img, i, j)
-
-			energies[i][j] = energy
-			if energy > max {
-				max = energy
-			}
-		}
-	}
-
-	seam := getHorizontalSeam(energies)
-
 	result := image.NewRGBA(img.Bounds())
 	for i := 0; i < len(energies); i++ {
 		for j := 0; j < len(energies[0]); j++ {
-			if j == seam[i] {
-				println(j)
-				result.Set(i, j, color.White)
-			} else {
-				result.Set(i, j, color.Gray16{0xffff - uint16((energies[i][j]/max)*0xffff)})
+			result.Set(i, j, img.At(i, j))
+			energies[i][j] = L.ComputeEnergy(img, i, j)
+		}
+	}
+
+	maxSteps, err := strconv.Atoi(r.FormValue("max_steps"))
+	if err != nil {
+		maxSteps = 1
+	}
+
+	horizontalDynamic := getDynamicPrepResult(energies, maxSteps)
+
+	for i := 0; i < img.Bounds().Dy()-height; i++ {
+		seam := getHorizontalSeam(horizontalDynamic, maxSteps)
+		for j := 0; j < len(seam); j++ {
+			horizontalDynamic[j][seam[j]] = math.Inf(1)
+		}
+		for k := 0; k < len(horizontalDynamic)-1; k++ {
+			left, right := 0, 0
+			for right < len(horizontalDynamic[k]) {
+				if horizontalDynamic[k][right] != math.Inf(1) {
+					tempDyn := horizontalDynamic[k][right]
+					horizontalDynamic[k][right] = horizontalDynamic[k][left]
+					horizontalDynamic[k][left] = tempDyn
+
+					tempResult := result.At(k, right)
+					result.Set(k, right, result.At(k, left))
+					result.Set(k, left, tempResult)
+
+					left++
+				}
+				right++
 			}
 		}
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/octet-stream")
-	png.Encode(w, result.SubImage(result.Rect))
-
-	// width, err := strconv.Atoi(r.FormValue("width"))
-	// if err != nil {
-	// 	http.Error(w, "Invalid width value", http.StatusBadRequest)
-	// }
-
-	// height, err := strconv.Atoi(r.FormValue("height"))
-	// if err != nil {
-	// 	http.Error(w, "Invalid height value", http.StatusBadRequest)
-	// }
-
-	//fmt.Fprintf(w, "Image size: %d - width, %d - height", img.Bounds().Dx(), img.Bounds().Dy())
+	png.Encode(w, result.SubImage(image.Rectangle{Min: image.Pt(0, 0), Max: image.Pt(width, height)}))
 }
 
-func getHorizontalSeam(energies [][]float64) []int {
+func getHorizontalSeam(dynamic [][]float64, maxStep int) []int {
+	result := make([]int, len(dynamic))
+	result[len(dynamic)-1] = func() int {
+		min, minIndex := math.Inf(1), 0
+		for i := 0; i < len(dynamic[len(dynamic)-1]); i++ {
+			temp := dynamic[len(dynamic)-1][i]
+			if temp < min {
+				min = temp
+				minIndex = i
+			}
+		}
+		return minIndex
+	}()
+	for i := len(dynamic) - 2; i >= 0; i-- {
+		minIndex, min := 0, math.Inf(1)
+		for k := 0; k < (maxStep*2 + 1); k++ {
+
+			tempIndex := int(math.Max(float64((result[i+1]-maxStep+k)%(len(dynamic[i])-1)), 0))
+			temp := dynamic[i][tempIndex]
+			if temp < min {
+				min = temp
+				minIndex = tempIndex
+			}
+		}
+		result[i] = minIndex
+	}
+
+	return result
+}
+
+func getDynamicPrepResult(energies [][]float64, maxStep int) [][]float64 {
 	// creating 2-dimentional array filled with +Inf
 	dynamic := make([][]float64, len(energies))
 	for i := 0; i < len(dynamic); i++ {
 		dynamic[i] = make([]float64, len(energies[0]))
-		dynamic[i][0] = energies[i][0]
-		for j := 1; j < len(dynamic[i]); j++ {
-			dynamic[i][j] = math.Inf(1)
+		for j := 0; j < len(dynamic[i]); j++ {
+			dynamic[i][j] = energies[i][j]
 		}
 	}
 
 	// using dynamic programming to get min cumulative energy for each point starting from top
 	for i := 1; i < len(dynamic); i++ {
 		for j := 0; j < len(dynamic[i]); j++ {
-			minValue := func() float64 {
-				if j == 0 {
-					return math.Min(dynamic[i-1][j], dynamic[i-1][j+1])
-				} else if j == (len(dynamic[i]) - 1) {
-					return math.Min(dynamic[i-1][j-1], dynamic[i-1][j])
-				} else {
-					return math.Min(dynamic[i-1][j-1], dynamic[i-1][j])
-				}
-			}()
-			dynamic[i][j] = energies[i][j] + minValue
+			min := math.Inf(1)
+			for k := 0; k < (maxStep*2 + 1); k++ {
+				min = math.Min(dynamic[i-1][int(math.Max(float64((j-maxStep+k)%(len(dynamic[i-1])-1)), 0))], min)
+			}
+			dynamic[i][j] = min + energies[i][j]
 		}
 	}
 
-	result := make([]int, len(energies))
-	for row := len(energies) - 1; row >= 0; row-- {
-		minIndex := 0
-		slice := func() []float64 {
-			if row > 0 {
-				if result[row-1] == 0 {
-					return energies[row][result[row-1] : result[row-1]+2]
-				} else if result[row-1] == len(energies)-1 {
-					return energies[row][result[row-1]-1 : result[row-1]+1]
-				} else {
-					return energies[row][result[row-1]-1 : result[row-1]+2]
-				}
-			} else {
-				return energies[row]
-			}
-		}()
-		for i, v := range slice {
-			if energies[row][minIndex] > v {
-				minIndex = i
-			}
-		}
-		result[row] = minIndex
-	}
-
-	return result
+	return dynamic
 }
